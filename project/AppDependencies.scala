54b225abac8cb2963a0fc4d297647c315d362722
Ed Cope <87376121+EdCope@users.noreply.github.com>
2024-01-12 10:47:07 +0000

    DDCE-5084: Play 3.0 Migration (#63)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,15 @@ import play.sbt.PlayImport.*
 import sbt.*
 
 object AppDependencies {
-  private lazy val bootStrapPlayVersion = "7.21.0"
+  private lazy val bootStrapPlayVersion = "8.4.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootStrapPlayVersion
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
-    "com.vladsch.flexmark" % "flexmark-all"           % "0.64.8",
-    "org.scalatest"       %% "scalatest"              % "3.2.16",
-    "uk.gov.hmrc"         %% "bootstrap-test-play-28" % bootStrapPlayVersion
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootStrapPlayVersion
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID]           = compile ++ test
