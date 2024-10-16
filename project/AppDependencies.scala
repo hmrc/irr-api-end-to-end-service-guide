06c2279f1fad786983b5820da6375803358b6626
Victor Osipov <osipovvictor1994@gmail.com>
2024-10-16 16:05:48 +0100

    DDCE-5881 - Upgrade to scala 3

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object AppDependencies {
-  private lazy val bootStrapPlayVersion = "9.1.0"
+  private lazy val bootStrapPlayVersion = "9.5.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootStrapPlayVersion
@@ -11,5 +11,5 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootStrapPlayVersion
   ).map(_ % Test)
 
-  def apply(): Seq[ModuleID]           = compile ++ test
+  def apply(): Seq[ModuleID] = compile ++ test
 }
